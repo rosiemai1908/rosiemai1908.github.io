2bd3d9639d64c47f8a8c761c38e92b733f2c731d
rosiemai1908 <main363@newschool.edu>
2022-03-03 11:17:01 -0500

    studio and lab commit

---
+++ Core interaction 2/Studio 2022/imagine/script.js
@@ -0,0 +1,39 @@
+function alert1() {
+    var popup = document.getElementById("pop1");
+    popup.classList.toggle("show");
+  }
+  
+  function alert2() {
+    var popup = document.getElementById("pop2");
+    popup.classList.toggle("show");
+  }
+  
+  function alert3() {
+    var popup = document.getElementById("pop3");
+    popup.classList.toggle("show");
+  }
+  
+  function alert4() {
+    var popup = document.getElementById("pop4");
+    popup.classList.toggle("show");
+  }
+  
+  function alert5() {
+    var popup = document.getElementById("pop5");
+    popup.classList.toggle("show");
+  }
+  
+  function alert6() {
+    var popup = document.getElementById("pop6");
+    popup.classList.toggle("show");
+  }
+  
+  function alert7() {
+    var popup = document.getElementById("pop7");
+    popup.classList.toggle("show");
+  }
+  
+  function alert8() {
+    var popup = document.getElementById("pop8");
+    popup.classList.toggle("show");
+  }
