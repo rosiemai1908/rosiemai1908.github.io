28c6e821bc8ce9ad8ee4894beaf0616e779e852a
rosiemai1908 <main363@newschool.edu>
2022-04-01 10:18:52 -0400

    portfolio

--- Core interaction 2/Lab 2022/bookshelf/script.js
+++ Core interaction 2/Lab 2022/bookshelf/script.js
@@ -1,1 +1,27 @@
 console.log("Hello, bookshelf");
+
+var Airtable = require("airtable");
+console.log(Airtable);
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyhIiKOMAQWNvsMt'}).base('app6ljwgKqItWDznC');
+
+base('books').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 10,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Name'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
